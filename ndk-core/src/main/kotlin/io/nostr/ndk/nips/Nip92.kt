b1df0a79bfbe8c2afbd4799968ecb6d6dbba76e7
pablof7z <pfer@me.com>
2025-12-11 15:36:10 +0200

    feat(ndk-core): add NIP-92 media attachment constants

---
+++ ndk-core/src/main/kotlin/io/nostr/ndk/nips/Nip92.kt
@@ -0,0 +1,18 @@
+package io.nostr.ndk.nips
+
+/**
+ * NIP-92: Media Attachments
+ * https://github.com/nostr-protocol/nips/blob/master/92.md
+ */
+const val KIND_IMAGE = 20
+
+/**
+ * NIP-71: Video Events
+ * https://github.com/nostr-protocol/nips/blob/master/71.md
+ */
+const val KIND_VIDEO = 34235
+
+/**
+ * Generic reply kind for replying to non-kind-1 events
+ */
+const val KIND_GENERIC_REPLY = 1111
