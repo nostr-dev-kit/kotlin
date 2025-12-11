431930ab9040d330a40c783b85f737588b1f5c2e
pablof7z <pfer@me.com>
2025-12-11 15:37:43 +0200

    fix(ndk-core): remove duplicate KIND_GENERIC_REPLY constant from Nip92

--- ndk-core/src/main/kotlin/io/nostr/ndk/nips/Nip92.kt
+++ ndk-core/src/main/kotlin/io/nostr/ndk/nips/Nip92.kt
@@ -11,8 +11,3 @@ const val KIND_IMAGE = 20
  * https://github.com/nostr-protocol/nips/blob/master/71.md
  */
 const val KIND_VIDEO = 34235
-
-/**
- * Generic reply kind for replying to non-kind-1 events
- */
-const val KIND_GENERIC_REPLY = 1111
